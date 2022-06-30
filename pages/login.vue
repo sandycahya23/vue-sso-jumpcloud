58e8a04098a26cda6c2c2b5a64c5c26ddce4ab4e
sandy.cahya <sandy.cahya@ovo.id>
2022-06-30 16:56:53 +0700

    add example for open api

--- pages/login.vue
+++ pages/login.vue
@@ -1,21 +1,42 @@
 <template>
-  <v-card class="justify-center">
+<div>
+  <v-card class="justify-center mb-8">
     <v-btn type="button" @click="loginWithJumpcloud">Login with Jumpcloud</v-btn>
+    <hr>
   </v-card>
+  <v-card>
+    <v-card-text>
+      <v-btn type="button" @click="fetchData"> Fetch Data</v-btn>
+      <p>{{data}}</p>
+    </v-card-text>
+  </v-card>
+</div>
 </template>
 <script>
+import axios from 'axios';
 export default {
   name: 'LoginPage',
   data() {
     return {
       popupWindow: null,
+      baseUrl: 'https://cat-fact.herokuapp.com/facts',
+      token: '',
+      data: '',
     }
   },
   methods: {
     loginWithJumpcloud() {
       this.newwindow = window.open('https://www.google.com', 'Jumpcloud login');
+      this.token = '';
       return false;
+    },
+    async fetchData() {
+      const response = await axios.get(this.baseUrl, { headers: {
+        Authorization: this.token,
+      }})
+      this.data = response.data;
     }
+
   }
 }
 </script>
