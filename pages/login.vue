37b0a01a55d12d3019f65b597ddc1253db05f6a9
sandy.cahya <sandy.cahya@ovo.id>
2022-06-30 16:45:41 +0700

    add tempalte for login page

---
+++ pages/login.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card class="justify-center">
+    <v-btn type="button" @click="loginWithJumpcloud">Login with Jumpcloud</v-btn>
+  </v-card>
+</template>
+<script>
+export default {
+  name: 'LoginPage',
+  data() {
+    return {
+      popupWindow: null,
+    }
+  },
+  methods: {
+    loginWithJumpcloud() {
+      this.newwindow = window.open('https://www.google.com', 'Jumpcloud login');
+      return false;
+    }
+  }
+}
+</script>
